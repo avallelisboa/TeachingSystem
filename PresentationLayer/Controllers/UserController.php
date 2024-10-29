ecb9ee5ebf3909d18e718efaefff8e50eddc65d3
Andrés <avallelisboa@gmail.com>
2024-10-29 17:46:09 -0400

    PHP loads react app

--- PresentationLayer/Controllers/UserController.php
+++ PresentationLayer/Controllers/UserController.php
@@ -3,7 +3,12 @@ require_once './ServiceLayer/Interfaces/ISessionService.php';
 function GetUserMainMenu($request,$response,$service, $app){
     $SESSION_SERVICE = $_ENV['SESSION_SERVICE'];
     $sessionService = SessionServiceFactory::getSessionService('php');
-    if($sessionService->isLogged())
-        $service->render('./PresentationLayer/Views/UserPage.php');
+    if($sessionService->isLogged()){
+        $environment = $_ENV['ENVIRONMENT'];
+        if($environment == "development")
+            $response->redirect('http://localhost:3000')->send();
+        else
+            $service->render('./PresentationLayer/Views/UserPage/public/index.html');
+    }
     else $response->redirect('/')->send();
 }
--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php
 require_once __DIR__ . '/vendor/autoload.php';
 
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
 require_once('./PresentationLayer/Controllers/LoginController.php');
 require_once('./PresentationLayer/Controllers/RegisterController.php');
 require_once('./PresentationLayer/Controllers/UserController.php');
