480680e70ba733cd81322e6362a10fa0ba812419
Andrés <avallelisboa@gmail.com>
2024-10-29 14:44:05 -0400

    change file name from UserMainPage to UserPage

--- PresentationLayer/Controllers/UserController.php
+++ PresentationLayer/Controllers/UserController.php
@@ -4,6 +4,6 @@ function GetUserMainMenu($request,$response,$service, $app){
     $SESSION_SERVICE = $_ENV['SESSION_SERVICE'];
     $sessionService = SessionServiceFactory::getSessionService('php');
     if($sessionService->isLogged())
-        $service->render('./PresentationLayer/Views/UserMainPage.php');
+        $service->render('./PresentationLayer/Views/UserPage.php');
     else $response->redirect('/')->send();
 }
